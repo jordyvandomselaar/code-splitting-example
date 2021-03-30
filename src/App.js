ea9a3073d38d017ca7c6986b4f4f14afbc28ffa4
Jordy van Domselaar <jordy.m.van.domselaar@gmail.com>
2021-03-30 22:25:44 +0200

    Use react.lazy & suspense to split code per page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import { Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
@@ -6,25 +6,27 @@ import GlobalStyles from './components/GlobalStyles';
 import './mixins/chartjs';
 import theme from './theme';
 import DashboardLayout from './components/DashboardLayout';
-import Account from './pages/Account';
-import Dashboard from './pages/Dashboard';
-import ProductList from './pages/ProductList';
-import Settings from './pages/Settings';
 import { Switch } from 'react-router';
 
-const App = () => {
+const Account = lazy(() => import('./pages/Account'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const ProductList = lazy(() => import('./pages/ProductList'));
+const Settings = lazy(() => import('./pages/Settings'));
 
+const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <DashboardLayout>
         <GlobalStyles />
-        <Switch>
-          <Route exact path="/" component={Dashboard}/>
-          <Route exact path="/app/account" component={Account}/>
-          <Route exact path="/app/dashboard" component={Dashboard}/>
-          <Route exact path="/app/products" component={ProductList}/>
-          <Route exact path="/app/settings" component={Settings}/>
-        </Switch>
+        <Suspense fallback={null}>
+          <Switch>
+            <Route exact path="/" component={Dashboard}/>
+            <Route exact path="/app/account" component={Account}/>
+            <Route exact path="/app/dashboard" component={Dashboard}/>
+            <Route exact path="/app/products" component={ProductList}/>
+            <Route exact path="/app/settings" component={Settings}/>
+          </Switch>
+        </Suspense>
       </DashboardLayout>
     </ThemeProvider>
   );
